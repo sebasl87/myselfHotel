8240ef5567bd742ba0476a476a3ba33d47d4539b
Sebastian <sebastianloguzzo@Sebastians-MacBook-Air.local>
2020-07-01 10:12:32 -0300

    ok myself con first

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDydsHFIZ5RJOG5OvInxbDwYmnlElbHUfY",
-    authDomain: "altosmyself.firebaseapp.com",
-    databaseURL: "https://altosmyself.firebaseio.com",
-    projectId: "altosmyself",
-    storageBucket: "altosmyself.appspot.com",
-    messagingSenderId: "419669849448",
-    appId: "1:419669849448:web:df5f80fd5e342c61b452dd",
-    measurementId: "G-6PVTCCBXH3"
+    apiKey: "AIzaSyBEIebmm3SNFt2FBTrUv75IKu17n1RCeFU",
+    authDomain: "ionic-login-e22c0.firebaseapp.com",
+    databaseURL: "https://ionic-login-e22c0.firebaseio.com",
+    projectId: "ionic-login-e22c0",
+    storageBucket: "ionic-login-e22c0.appspot.com",
+    messagingSenderId: "70700975268",
+    appId: "1:70700975268:web:a4dd4c59b226a3923a9231",
+    measurementId: "G-M0LV9HEL7T"
   }
 };
 
